7513d569ecad4593fbc401221baa53912563499b
Samitha <samitha.nillupul1440@gmail.com>
2023-12-23 18:20:18 +0530

    init commit

---
+++ Std_mng/src/main/java/com/example/std_mng/StdMngApplication.java
@@ -0,0 +1,13 @@
+package com.example.std_mng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdMngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StdMngApplication.class, args);
+    }
+
+}
---
+++ Std_mng/src/test/java/com/example/std_mng/StdMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.std_mng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdMngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
